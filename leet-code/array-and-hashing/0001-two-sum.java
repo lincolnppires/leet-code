300997cf13eee1206aea1dd45950f1535d5b67ae
lincolnppires <lincoln.ufop@gmail.com>
2024-10-30 20:11:41 -0300

    1 two sum

---
+++ leet-code/array-and-hashing/0001-two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> mapRemaining = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            var remain = target - nums[i];
+            if(mapRemaining.containsKey(remain) && mapRemaining.get(remain) != i) {
+                int[] ans = {i, mapRemaining.get(remain)};
+                return ans;
+            }
+            mapRemaining.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
