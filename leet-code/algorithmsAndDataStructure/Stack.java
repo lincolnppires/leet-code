6627a0b99f09431f137d7d4acfcc5da677c695c3
lincolnppires <lincoln.ufop@gmail.com>
2024-11-02 18:03:22 -0300

    feat: doubly linked list

--- leet-code/algorithmsAndDataStructure/Stack.java
+++ leet-code/algorithmsAndDataStructure/Stack.java
@@ -26,12 +26,4 @@ public class Stack {
         return stack.size();
     }
 
-    public static void main(String[] args) {
-        var stack = new Stack();
-        stack.push(1);
-        stack.push(2);
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-    }
 }
